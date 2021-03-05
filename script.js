2cfb723920f5cd6a14369f9a9b890bf7c15b0b7f
ddmeyer23 <78511312+ddmeyer23@users.noreply.github.com>
2021-03-04 19:20:52 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var map = L.map('map').setView([38.50,-90.324783], 11);
+
+  // load a tile layer
+ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+}).addTo(map);
